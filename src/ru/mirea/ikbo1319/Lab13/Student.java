b797f8ce58d3f465597f9b94c137c5599baa3da5
Maliutina Vitaliya <malavita29@mail.ru>
2020-10-21 23:05:27 +0300

    Init

---
+++ src/ru/mirea/ikbo1319/Lab13/Student.java
@@ -0,0 +1,23 @@
+package Lab13;
+
+public class Student {
+    private int srBall;
+
+    public Student(int srBall) {
+        this.srBall = srBall;
+    }
+
+    public int getSrBall() {
+        return srBall;
+    }
+
+
+    public Student() {
+
+    }
+
+    public void setSrBall(int ID) {
+        this.srBall = srBall;
+    }
+
+}
